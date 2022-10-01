bac7db3763e028b144edb5ab8a9e91b7da4408f7
Katie Cleland <kcleland0818@gmail.com>
2022-10-01 11:07:45 -0600

    Remove unused imports

--- src/main/java/CustomerDriver.java
+++ src/main/java/CustomerDriver.java
@@ -1,7 +1,3 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
